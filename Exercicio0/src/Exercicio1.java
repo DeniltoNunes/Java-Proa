828c7123d3353a33e230411570600628ba1cd6ed
Denilton Nunes <denilton.nunesm@gmail.com>
2021-12-08 11:43:08 -0300

    Correção de Bugs.
    Correção de bugs do arquivo Exercicio1.java

--- Exercicio0/src/Exercicio1.java
+++ Exercicio0/src/Exercicio1.java
@@ -1,4 +1,4 @@
-public class App {
+public class Exercicio1 {
     public static void main(String[] args) throws Exception {
         String nomeDoCarro = "Ferrari";
         System.out.println(nomeDoCarro);
