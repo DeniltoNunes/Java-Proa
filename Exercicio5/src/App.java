1e2409c0279411374cfd6b8b64bbfc17b0f7d4d5
Denilton Nunes <denilton.nunesm@gmail.com>
2021-12-08 11:34:28 -0300

    Upload Exercício 5.
    Upload do Exercício 5 na linguagem Java.

---
+++ Exercicio5/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Double valor = (double) 190;
+        Double hora = (double) 60;
+        Double resultado = valor / hora;
+        System.out.println(resultado);
+    }
+}
